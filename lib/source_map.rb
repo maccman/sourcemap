0cb9d2f4ae45abe9d75014cde4f83addb069b79e
Joshua Peek <josh@joshpeek.com>
2013-07-27 20:42:50 -0700

    require it all

--- lib/source_map.rb
+++ lib/source_map.rb
@@ -1,2 +1,6 @@
-module SourceMap
-end
+require 'source_map/map'
+require 'source_map/mapping'
+require 'source_map/mappings'
+require 'source_map/offset'
+require 'source_map/version'
+require 'source_map/vlq'
